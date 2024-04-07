66757896614c3c2b7fa70361ea9e79d5824bcfe7
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-07 19:03:30 +0000

    trabajando con react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,30 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import Card from"./card";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
 //create your first component
 const Home = () => {
 	return (
+		<div className="container  justify-content-center">
+		<Navbar/>
+		<Jumbotron/>
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
+			
+			<div className="row">
+			<div className="col-3"><Card /></div>
+			<div className="col-3"><Card /></div>
+			<div className="col-3"><Card /></div>
+			<div className="col-3"><Card /></div>
+						
+			</div>
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+		</div>
 		</div>
 	);
 };
